f4a8fcfa5a6e8d6513d6f6fe2253768e9413cd60
Matheus Lessa <matheus.lessa@sptech.school>
2024-05-08 00:07:22 -0300

    DateTime no Dotnet

--- Program.cs
+++ Program.cs
@@ -1,16 +1,21 @@
 ﻿using Fundamentos_.NET.Models;
 
-string apresentacao = "Olá, seja bem vindo";
-int quantidade = 1;
-double altura = 1.90;
-decimal preco = 1.80M;
-bool condicao = true;
+// Pega a data da maquina atual
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
 
-Console.WriteLine(apresentacao);
-Console.WriteLine("Valor da variavel qtd: " + quantidade);
-Console.WriteLine("Valor da variavel altura: " + altura.ToString("0.00"));
-Console.WriteLine("Valor da variavel preco: " + preco);
-Console.WriteLine("Valor da varieavel condição: " + condicao);
+// string apresentacao = "Olá, seja bem vindo";
+// int quantidade = 1;
+// double altura = 1.90;
+// decimal preco = 1.80M;
+// // Boolean
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variavel qtd: " + quantidade);
+// Console.WriteLine("Valor da variavel altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variavel preco: " + preco);
+// Console.WriteLine("Valor da varieavel condição: " + condicao);
 
 //  Pessoa pessoal = new Pessoa();
 
